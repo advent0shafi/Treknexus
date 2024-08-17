d982aba0bf486d0a8a1e4d0bb8263d0a3cc5b9a4
Mohammed Shafi Mundassery <129031512+advent0shafi@users.noreply.github.com>
2024-08-17 10:11:03 +0530

    Update info.py

--- e_comm/e_comm/info.py
+++ e_comm/e_comm/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'       
 EMAIL_HOST_USER = 'joytreknex@gmail.com'
-EMAIL_HOST_PASSWORD = 'cdaelsydgonahpxe'
+EMAIL_HOST_PASSWORD = 'password of the jmail.com'
 EMAIL_PORT = 587
